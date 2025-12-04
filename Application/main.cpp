45b6060e600bc75e9be9ec5ba214bff8110f97e9
Eng.Sara Saad <161247053+SaraSaadMohamud@users.noreply.github.com>
2025-12-04 02:07:28 +0200

    Update main.cpp

--- Application/main.cpp
+++ Application/main.cpp
@@ -6,64 +6,86 @@
 #include "../HAL/MPU9250_HAL.hpp"
 #include "../Service/MPU9250_Service.hpp"
 
-int main() {
+#define MPU9250_BAUD_RATE   400000
+
+int main() 
+{
     stdio_init_all();
+
     sleep_ms(200); // wait for USB serial
 
-    printf("Pico W MPU9250 I2C - Raw Sensor Reading Example\n");
-
-    const uint32_t I2C_BAUD = 400000;
+    printf("Start Pico W MPU9250 Sensor... \n");
 
     // HAL instance
-    MPU9250_HAL sensor(i2c_default, MPU6500_DEFAULT_ADDRESS);
+    MPU9250_HAL imu_hal(i2c_default, MPU6500_DEFAULT_ADDRESS);
+    IMUService imu_service(imu_hal);
 
-    if(!sensor.begin(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, I2C_BAUD)) {
-        printf("sensor begin failed! Check wiring.\n");
-        while(1) sleep_ms(500);
-    }
+    do
+    {
+        if(imu_hal.begin(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, MPU9250_BAUD_RATE))
+        {
+            printf("MPU9250 Connected successfully^^\n");
+            break;
+        } 
+        printf("MPU9250 Connected Failed!\n");
+        sleep_ms(500);
+    } while (1);
 
-    printf("MPU9250 detected. Initializing...\n");
+    do
+    {
+        if(imu_hal.initMPU9250())
+        {
+            printf("MPU9250 Init successfully^^\n");
+            break;
+        } 
+        printf("MPU9250 Configuration Failed!\n");
+        sleep_ms(500);
+    } while (1);
 
-    if(!sensor.initMPU6500()) {
-        printf("MPU6500 init failed!\n");
-        while(1) sleep_ms(500);
-    }
+    do
+    {
+        if(imu_hal.initAK8963())
+        {
+            printf("AK8963 Init successfully^^\n");
+            break;
+        } 
+        printf("AK8963 Configuration Failed!\n");
+        sleep_ms(500);
+    } while (1);
 
-    if(!sensor.initAK8963()) {
-        printf("AK8963 (Magnetometer) init failed!\n");
-        while(1) sleep_ms(500);
-    }
-
-    IMUService imu(sensor);
-
-    if(!imu.begin()) {
-        printf("IMUService begin failed!\n");
-        while(1) sleep_ms(500);
-    }
+    do
+    {
+        if(imu_service.begin())
+        {
+            printf("MPU9250 begin successfully^^\n");
+            break;
+        } 
+        printf("MPU9250 begin Failed!\n");
+        sleep_ms(500);
+    } while (1);
 
     printf("Initialization complete.\n\n");
 
-    const uint32_t loop_ms = 100; // 10Hz
+    while (true)
+    {
 
-    while (true) {
-
-        AccelData acc = imu.getAccelerometer();
-        GyroData  gyr = imu.getGyroscope();
-        TempData  tmp = imu.getTemperature();
-        MagData   mag = imu.getMagnetometer();
+        AccelData acc = imu_service.getAccelerometer();
+        GyroData  gyr = imu_service.getGyroscope();
+        TempData  tmp = imu_service.getTemperature();
+        //MagData   mag = imu.getMagnetometer();
 
         printf(
-            "ACC(g): %.3f  %.3f  %.3f | "
-            "GYR(dps): %.2f  %.2f  %.2f | "
-            "TEMP(C): %.2f | "
-            "MAG(uT): %.2f  %.2f  %.2f\n",
-            acc.x_g, acc.y_g, acc.z_g,
+            "ACC(g):    %.3f  %.3f  %.3f | "
+            "GYR(dps):  %.2f  %.2f  %.2f | "
+            "TEMP(C):   %.2f  "
+            //"| MAG(uT): %.2f  %.2f  %.2f\n",
+            ,acc.x_g, acc.y_g, acc.z_g,
             gyr.x_dps, gyr.y_dps, gyr.z_dps,
-            tmp.temperature_c,
-            mag.x_uT, mag.y_uT, mag.z_uT
+            tmp.temperature_c
+            //mag.x_uT, mag.y_uT, mag.z_uT
         );
 
-        sleep_ms(loop_ms);
+        sleep_ms(100);
     }
 
     return 0;
