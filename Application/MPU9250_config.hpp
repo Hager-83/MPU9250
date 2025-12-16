9e492a599b31dac8d734f1eb2d8e940ab4f86409
Eng.Sara Saad <161247053+SaraSaadMohamud@users.noreply.github.com>
2025-12-16 12:49:28 +0200

    Update MPU9250_config.hpp

--- Application/MPU9250_config.hpp
+++ Application/MPU9250_config.hpp
@@ -8,21 +8,13 @@
 #include "hardware/gpio.h"
 #include "HAL/MPU9250_HAL.hpp"
 
-/**************************** Macros ******************************* */
-#define I2C0_SDA_PIN 0
-#define I2C0_SCL_PIN 1
-#define I2C1_SDA_PIN 2
-#define I2C1_SCL_PIN 3
-#define BAUD_RATE 400000
-
 /****************************************************************** */
 namespace MPU9250_Config
 {
     // -------------- I2C Settings ---------------
-    constexpr uint32_t Baud_Rate = BAUD_RATE ;
-    constexpr uint8_t SDA_PIN = I2C0_SDA_PIN ;
-    constexpr uint8_t SCL_PIN = I2C0_SCL_PIN;
-    constexpr uint32_t I2C_BAUDRATE = Baud_Rate;
+    constexpr uint8_t SDA_PIN = 4;
+    constexpr uint8_t SCL_PIN = 5;
+    constexpr uint32_t kBaudRate = 400000;
 
     // -------------------------- MPU9250 Address -------------------------
     constexpr uint8_t MPU_ADDRESS = MPU6500_DEFAULT_ADDRESS;
@@ -46,7 +38,7 @@ namespace MPU9250_Config
         init.address     = MPU_ADDRESS;
         init.sda_pin     = SDA_PIN;
         init.scl_pin     = SCL_PIN;
-        init.baudrate_hz = I2C_BAUDRATE;
+        init.baudrate_hz = kBaudRate;
         return init;
     }
 }
